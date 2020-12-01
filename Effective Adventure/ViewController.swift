c6225cd89b367e698a71b4547276bb01b101f1a7
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-12-02 00:33:37 +0530

    Initial Commit

---
+++ Effective Adventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Effective Adventure
+//
+//  Created by MACBOOK on 02/12/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
